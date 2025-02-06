12a6a4db933f992d9696bc22bf4ce9b871faee0a
SC1314520 <g0710126@hchs.hc.edu.tw>
2025-02-06 19:31:40 +0800

    commit

---
+++ src/pages/Main/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>{{text}}</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name:'Main',
+  data(){
+    return {
+      text:''
+    }
+  },
+  mounted(){
+    axios.get('/run/main')
+    .then((resp)=>{
+      this.text = resp.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
